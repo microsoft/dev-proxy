1686c867328daf9df57264a4dc6b8d707307f228
waldekmastykarz <waldek@mastykarz.nl>
2022-09-22 08:17:18 +0200

    Sets GET as a default method. Closes #17

--- msgraph-chaos-proxy/ChaosProxyConfiguration.cs
+++ msgraph-chaos-proxy/ChaosProxyConfiguration.cs
@@ -45,8 +45,9 @@ namespace Microsoft.Graph.ChaosProxy {
                 var responsesString = File.ReadAllText(_responsesFilePath);
                 var responsesConfig = JsonSerializer.Deserialize<ChaosProxyConfiguration>(responsesString);
                 IEnumerable<ChaosProxyMockResponse>? configResponses = responsesConfig?.Responses;
-                if (configResponses is not null)
+                if (configResponses is not null) {
                     Responses = configResponses;
+                }
             }
             catch (Exception ex)
             {
@@ -97,7 +98,7 @@ namespace Microsoft.Graph.ChaosProxy {
         [JsonPropertyName("url")]
         public string Url { get; set; } = string.Empty;
         [JsonPropertyName("method")]
-        public string Method { get; set; } = string.Empty;
+        public string Method { get; set; } = "GET";
         [JsonPropertyName("responseCode")]
         public int? ResponseCode { get; set; } = 200;
         [JsonPropertyName("responseBody")]
