e77497276221c4a90c44f1956f1b4f7baef51a96
waldekmastykarz <waldek@mastykarz.nl>
2022-09-23 09:58:27 +0200

    Removes 'Microsoft' from the AAD app name in setup.sh

--- samples/scripts/setup.sh
+++ samples/scripts/setup.sh
@@ -6,7 +6,7 @@ npx -p @pnp/cli-microsoft365 -- m365 login --authType browser
 
 # create AAD app
 echo "Creating AAD app..."
-appId=$(npx -p @pnp/cli-microsoft365 -- m365 aad app add --name "Microsoft Graph Chaos Proxy Samples" --multitenant --redirectUris "http://localhost:5500/withsdk.html,http://localhost:5500/nosdk.html" --apisDelegated "https://graph.microsoft.com/User.Read.All,https://graph.microsoft.com/Presence.Read.All" --platform spa --query "appId" -o text)
+appId=$(npx -p @pnp/cli-microsoft365 -- m365 aad app add --name "Graph Chaos Proxy Samples" --multitenant --redirectUris "http://localhost:5500/withsdk.html,http://localhost:5500/nosdk.html" --apisDelegated "https://graph.microsoft.com/User.Read.All,https://graph.microsoft.com/Presence.Read.All" --platform spa --query "appId" -o text)
 
 # write app to env.js
 echo "Writing app to env.js..."
