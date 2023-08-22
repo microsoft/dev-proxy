360c956233f80daed8b0e31739e8dc6b36ce01f2
Garry Trinder <garry@trinder365.co.uk>
2023-08-22 08:41:22 +0100

    Improve error handling when using options from disabled plugins. Closes #289 (#317)
    * Improve error handling when using options from disabled plugins. Closes #289
    
    * Address review comments
    
    * Fix help and version
    
    * Address review comments
    
    ---------
    
    Co-authored-by: Waldek Mastykarz <waldek@mastykarz.nl>

--- m365-developer-proxy/Program.cs
+++ m365-developer-proxy/Program.cs
@@ -10,6 +10,7 @@ ILogger logger = new ConsoleLogger(ProxyCommandHandler.Configuration, pluginEven
 IProxyContext context = new ProxyContext(logger, ProxyCommandHandler.Configuration);
 ProxyHost proxyHost = new();
 RootCommand rootCommand = proxyHost.GetRootCommand();
+
 PluginLoaderResult loaderResults = new PluginLoader(logger).LoadPlugins(pluginEvents, context);
 
 // have all the plugins init and provide any command line options
@@ -17,4 +18,42 @@ pluginEvents.RaiseInit(new InitArgs(rootCommand));
 
 rootCommand.Handler = proxyHost.GetCommandHandler(pluginEvents, loaderResults.UrlsToWatch, logger);
 
-return await rootCommand.InvokeAsync(args);
+// store the global options that are created automatically for us
+// rootCommand doesn't return the global options, so we have to store them manually
+string[] globalOptions = { "--version", "--help", "-h", "/h", "-?", "/?" };
+
+// filter args to retrieve options
+var incomingOptions = args.Where(arg => arg.StartsWith("-") || arg.StartsWith("/")).ToArray();
+
+// remove the global options from the incoming options
+incomingOptions = incomingOptions.Except(globalOptions).ToArray();
+
+// compare the incoming options against the root command options
+foreach (var option in rootCommand.Options)
+{
+    // get the option aliases
+    var aliases = option.Aliases.ToArray();
+
+    // iterate over aliases
+    foreach (string alias in aliases)
+    {
+        // if the alias is present
+        if (incomingOptions.Contains(alias))
+        {
+            // remove the option from the incoming options
+            incomingOptions = incomingOptions.Where(val => val != alias).ToArray();
+        }
+    }
+}
+
+// list the remaining incoming options as unknown in the output
+if (incomingOptions.Length > 0)
+{
+    logger.LogError($"Unknown option(s): {string.Join(" ", incomingOptions)}");
+    logger.LogInfo($"TIP: Use --help view available options");
+    logger.LogInfo($"TIP: Are you missing a plugin? See: https://aka.ms/m365/proxy/plugins");
+}
+else
+{
+    await rootCommand.InvokeAsync(args);
+}
