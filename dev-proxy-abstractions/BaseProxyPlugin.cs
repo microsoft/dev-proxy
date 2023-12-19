7e946450ee086329db2afe2f42f9cd6373268d80
waldekmastykarz <waldek@mastykarz.nl>
2023-12-19 14:21:06 +0100

    Adds one more file

--- dev-proxy-abstractions/BaseProxyPlugin.cs
+++ dev-proxy-abstractions/BaseProxyPlugin.cs
@@ -7,32 +7,32 @@ namespace Microsoft.DevProxy.Abstractions;
 
 public abstract class BaseProxyPlugin : IProxyPlugin
 {
-  protected ISet<UrlToWatch>? _urlsToWatch;
-  protected ILogger? _logger;
+    protected ISet<UrlToWatch>? _urlsToWatch;
+    protected ILogger? _logger;
 
-  public virtual string Name => throw new NotImplementedException();
-  public virtual void Register(IPluginEvents pluginEvents,
-                       IProxyContext context,
-                       ISet<UrlToWatch> urlsToWatch,
-                       IConfigurationSection? configSection = null)
-  {
-    if (pluginEvents is null)
+    public virtual string Name => throw new NotImplementedException();
+    public virtual void Register(IPluginEvents pluginEvents,
+                         IProxyContext context,
+                         ISet<UrlToWatch> urlsToWatch,
+                         IConfigurationSection? configSection = null)
     {
-      throw new ArgumentNullException(nameof(pluginEvents));
+        if (pluginEvents is null)
+        {
+            throw new ArgumentNullException(nameof(pluginEvents));
+        }
+
+        if (context is null || context.Logger is null)
+        {
+            throw new ArgumentException($"{nameof(context)} must not be null and must supply a non-null Logger", nameof(context));
+
+        }
+
+        if (urlsToWatch is null || urlsToWatch.Count == 0)
+        {
+            throw new ArgumentException($"{nameof(urlsToWatch)} cannot be null or empty", nameof(urlsToWatch));
+        }
+
+        _urlsToWatch = urlsToWatch;
+        _logger = context.Logger;
     }
-
-    if (context is null || context.Logger is null)
-    {
-      throw new ArgumentException($"{nameof(context)} must not be null and must supply a non-null Logger", nameof(context));
-
-    }
-
-    if (urlsToWatch is null || urlsToWatch.Count == 0)
-    {
-      throw new ArgumentException($"{nameof(urlsToWatch)} cannot be null or empty", nameof(urlsToWatch));
-    }
-
-    _urlsToWatch = urlsToWatch;
-    _logger = context.Logger;
-  }
 }
