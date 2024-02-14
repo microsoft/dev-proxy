962657efd5bf0798b3987f6416ce588ace284a90
Waldek Mastykarz <waldek@mastykarz.nl>
2024-02-14 08:13:46 +0100

    Updates macOS setup scripts to not fail on xattr (#560)
    * Updates macOS setup scripts to not fail on xattr
    
    * Update setup-beta.sh
    
    * Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -70,8 +70,6 @@ unzip -o ./devproxy.zip -d ./
 rm ./devproxy.zip
 echo "Configuring devproxy and its files as executable..."
 chmod +x ./devproxy ./libe_sqlite3.dylib
-echo "Removing quarantine attribute from devproxy and its files..."
-xattr -d com.apple.quarantine ./devproxy ./libe_sqlite3.dylib
 
 echo "Adding devproxy to the PATH environment variable in your shell profile..."
 
