a6e0d1518a8bfe631921c39c6929e0719559a1c7
Garry Trinder <garry@trinder365.co.uk>
2024-04-18 17:34:11 +0100

    Adds outdated command. Closes #657

---
+++ dev-proxy/OutdatedCommandHandler.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Microsoft.DevProxy;
+
+public static class OutdatedCommandHandler
+{
+    public static async Task CheckVersion(ILogger logger)
+    {
+        var releaseInfo = await UpdateNotification.CheckForNewVersion(ProxyCommandHandler.Configuration.NewVersionNotification);
+        logger.LogInformation(releaseInfo?.Version);
+    }
+}
+
