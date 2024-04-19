9fb6037685d8c21703c21bce9055ad76ef6c1948
Garry Trinder <garry@trinder365.co.uk>
2024-04-19 16:05:44 +0100

    Adds --short option to outdated command

--- dev-proxy/OutdatedCommandHandler.cs
+++ dev-proxy/OutdatedCommandHandler.cs
@@ -1,15 +1,32 @@
 ﻿using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Extensions;
+using System.Runtime.InteropServices;
 
 namespace Microsoft.DevProxy;
 
 public static class OutdatedCommandHandler
 {
-    public static async Task CheckVersion(ILogger logger)
+    public static async Task CheckVersion(bool versionOnly, ILogger logger)
     {
         var releaseInfo = await UpdateNotification.CheckForNewVersion(ProxyCommandHandler.Configuration.NewVersionNotification);
-        if (releaseInfo != null)
+
+        if (releaseInfo != null && releaseInfo.Version != null)
         {
-            logger.LogInformation(releaseInfo.Version);
+            var isBeta = releaseInfo.Version.Contains("-beta");
+
+            if (versionOnly)
+            {
+                logger.LogInformation(releaseInfo.Version);
+            }
+            else
+            {
+                var notesLink = isBeta ? "https://aka.ms/devproxy/notes" : "https://aka.ms/devproxy/beta/notes";
+                logger.LogInformation($"New Dev Proxy version {releaseInfo.Version} is available.\r\nRelease notes: {notesLink} \r\nDocs: https://aka.ms/devproxy/upgrade");
+            }
+        }
+        else if (!versionOnly)
+        {
+            logger.LogInformation("You are using the latest version of Dev Proxy.");
         }
     }
 }
