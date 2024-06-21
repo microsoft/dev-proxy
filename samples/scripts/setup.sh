7200cfe9d9afc839a0358b3d3169cae23a75a826
Garry Trinder <garry@trinder365.co.uk>
2024-06-21 11:53:09 +0100

    chore: Update app name in setup scripts

--- samples/scripts/setup.sh
+++ samples/scripts/setup.sh
@@ -5,7 +5,7 @@ echo "Sign in to Microsoft 365..."
 npx -p @pnp/cli-microsoft365 -- m365 login --authType browser
 
 # create AAD app
-echo "Creating Entra ID app..."
+echo "Creating Entra app..."
 appId=$(npx -p @pnp/cli-microsoft365 -- m365 entra app add --name graph-developer-proxy-samples --multitenant --redirectUris http://localhost:3000/withsdk.html,http://localhost:3000/nosdk.html --apisDelegated https://graph.microsoft.com/User.Read.All,https://graph.microsoft.com/Presence.Read.All --grantAdminConsent --platform spa --query appId -o text)
 
 # write app to env.js
